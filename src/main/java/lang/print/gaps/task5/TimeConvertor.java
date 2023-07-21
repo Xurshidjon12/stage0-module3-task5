eea326c3606c6130ff7aed22970cbdc1b634ebd1
Stanislau_Sukora <ssukora@estee.com>
2023-07-21 10:43:50 +0300

    Misc updates

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60;
+        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor convertor = new TimeConvertor();
+        float minutesToConvert = 5.5f;
+        convertor.convert(minutesToConvert);
     }
 }
