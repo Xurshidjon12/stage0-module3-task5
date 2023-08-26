b913d2383148bcc7238e7f566c17c24110ad213b
Xurshidjon12 <xurshidjontoshtemirov50@gmail.com>
2023-08-26 09:54:02 +0500

    Merge remote-tracking branch 'origin/main'

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,14 +1,16 @@
 package lang.print.gaps.task5;
 
+import java.util.logging.Logger;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
         int result = toBeDivided / 5;
-        System.out.println("Result of division: " + result);
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
         NumberDivider divider = new NumberDivider();
-        int numberToDivide = 20;
+        int numberToDivide = 25;
         divider.divide(numberToDivide);
     }
 }
